cf8a74765d5fed90969d1505680e8d1aa5bf1229
Neil Dudani <neil.dudani@gmail.com>
2021-07-10 12:12:35 -0400

    Initial changes to copying files

--- schema.sql
+++ schema.sql
@@ -19,12 +19,12 @@ CREATE TABLE users (
 
 CREATE TABLE shops (
   shop_id serial PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+  name VARCHAR(100) NOT NULL,
   address VARCHAR(80) NOT NULL,
   city VARCHAR(80) NOT NULL,
-  state VARCHAR(4) NOT NULL,
+  state VARCHAR(4),
   zip INT NOT NULL,
-  date timestamp,
+  date TEXT,
   phone_number TEXT,
   website TEXT,
   animal_friendly boolean
@@ -34,12 +34,12 @@ CREATE TABLE reviews (
   review_id serial PRIMARY KEY,
   user_id INT NOT NULL,
   shop_id INT NOT NULL,
-  summary VARCHAR(50) NOT NULL,
+  summary TEXT NOT NULL,
   category VARCHAR(30) NOT NULL,
   description TEXT NOT NULL,
   rating INT,
   owner_response TEXT,
-  date timestamp,
+  date TEXT,
   helpfulness_count INT DEFAULT 0,
 
   CONSTRAINT fk_user
@@ -77,3 +77,27 @@ CREATE INDEX shops_photos_shop_id_index ON shops_photos(shop_id);
 CREATE INDEX reviews_photos_review_id_index ON reviews_photos(review_id);
 
 -- INSERT INTO DB
+COPY users(user_id, first_name, last_name, email, password, address,city, state, zip, profile_photo_url)
+FROM '/Users/neildudani/Desktop/Neil/HackReactor/Immersive/Projects/BlueOcean/DB/data/users.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY shops(shop_id, name, address, city, state, zip, date,phone_number, website, animal_friendly)
+FROM '/Users/neildudani/Desktop/Neil/HackReactor/Immersive/Projects/BlueOcean/DB/data/shops.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY reviews(review_id, user_id, shop_id, summary, category,description, rating, owner_response, date, helpfulness_count)
+FROM '/Users/neildudani/Desktop/Neil/HackReactor/Immersive/Projects/BlueOcean/DB/data/reviews.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY shops_photos(photo_id, shop_id, url)
+FROM '/Users/neildudani/Desktop/Neil/HackReactor/Immersive/Projects/BlueOcean/DB/data/shops_photos.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY reviews_photos(photo_id, review_id, url)
+FROM '/Users/neildudani/Desktop/Neil/HackReactor/Immersive/Projects/BlueOcean/DB/data/reviews_photos.csv'
+DELIMITER ','
+CSV HEADER;
